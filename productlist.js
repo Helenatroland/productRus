6a106e75a9240614ff60216c8d50d195a553ec4a
Helena Troland <hetr0001@stud.kea.dk>
2025-02-03 12:45:51 +0100

    update15

--- productlist.js
+++ productlist.js
@@ -1,23 +1,23 @@
 let productContainer = document.querySelector(".product_list_container");
 
-fetch(`https://kea-alt-del.dk/t7/api/products/`)
+fetch(`https://kea-alt-del.dk/t7/api/products?limit=100`)
   .then((response) => response.json())
   .then((data) => showList(data));
 
 function showList(products) {
   console.log(products);
-  let markup = "";
-  products
-    .map((product) => {
-      markup += `
+  const markup = products
+    .map(
+      (product) => `
     <div class="blaa_tshirt">
       <img src="https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp" alt="blaa_tshirt" />
       <h3>${product.productdisplayname}</h3>
       <p>Tshirts - Nike</p>
       <p>DKK${product.price},-</p>
       <a href="product.html">Read More</a>
-    </div>`;
-    })
+    </div>
+    `
+    )
     .join("");
   console.log(markup);
   productContainer.innerHTML = markup;
